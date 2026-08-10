902e65fa5dc3cbfe9e7fb9238a9cc26025b724ae
yashpatel521 <yash1451999@gmail.com>
2026-08-10 14:25:27 -0400

    style: change Sonner Toaster position from top-right to bottom-right

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -114,7 +114,7 @@ export default function RootLayout({
       </head>
       <body className={`${inter.className} bg-[#0c0a09] text-slate-100 min-h-screen antialiased flex flex-col p-0 m-0 overflow-x-hidden`}>
         <AppLayout>{children}</AppLayout>
-        <Toaster position="top-right" theme="dark" richColors />
+        <Toaster position="bottom-right" theme="dark" richColors />
       </body>
     </html>
   );
